9753a1ca52e260ed61600ddae4e38ed95f7f1043
neethujustin <bwithneethu@gmail.com>
2022-01-13 17:20:29 +0530

    Register UI validations

--- src/app/account/register.component.ts
+++ src/app/account/register.component.ts
@@ -22,12 +22,12 @@ export class RegisterComponent implements OnInit {
 
     ngOnInit() {
         this.form = this.formBuilder.group({
-            firstName: ['', [Validators.required, Validators.maxLength(25)]],
+            firstName: ['', [Validators.required, Validators.maxLength(20)]],
             middleName: ['', Validators.nullValidator],
-            lastName: ['', Validators.required,Validators.maxLength(25)],
+            lastName: ['', [Validators.required,Validators.maxLength(20)]],
             empId: ['', [Validators.required,Validators.pattern(/^-?([0-9]\d*)?$/)]],
             email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
-            password: ['', [Validators.required, Validators.minLength(6)]]
+            password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(15)]]
         });
     }
 
@@ -39,23 +39,24 @@ export class RegisterComponent implements OnInit {
 
         // reset alerts on submit
         this.alertService.clear();
-
+        console.log(this.form)
         // stop here if form is invalid
         if (this.form.invalid) {
             return;
         }
-
-        this.loading = true;
-        this.accountService.register(this.form.value)
-            .pipe(first())
-            .subscribe(
-                data => {
-                    this.alertService.success('Registration successful', { keepAfterRouteChange: true });
-                    this.router.navigate(['../login'], { relativeTo: this.route });
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false;
-                });
+            this.loading = true;
+            this.accountService.register(this.form.value)
+                .pipe(first())
+                .subscribe(
+                    data => {
+                        this.alertService.success('Registration successful', { keepAfterRouteChange: true });
+                        this.router.navigate(['../login'], { relativeTo: this.route });
+                    },
+                    error => {
+                        this.alertService.error(error);
+                        this.loading = false;
+                    });
+        
+       
     }
 }
